e2310d4b80293a0253eafbf031a7e8105ade67b3
K-CHANHO <cksgh1565@gmail.com>
2024-06-17 17:49:30 +0900

    JWT 토큰 서비스 구현중

--- src/main/java/sideProject/diary/jwt/JwtService.java
+++ src/main/java/sideProject/diary/jwt/JwtService.java
@@ -1,5 +1,37 @@
 package sideProject.diary.jwt;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Service
 public class JwtService {
+    private final Key key;
+
+    public JwtService(@Value("${jwt.secret}")String secretKey){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public JwtDTO generateToken(Authentication authentication) {
+
+        // 권한 가져오기
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+        long accessTokenValidDay = 3;
+
+
+    }
+
 
 }
